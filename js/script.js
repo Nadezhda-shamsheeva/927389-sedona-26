443560128cc541659938ea05d95cd643949e7b64
Nadezhda-shamsheeva <53871646+Nadezhda-shamsheeva@users.noreply.github.com>
2019-10-28 21:05:36 +0200

    Дописан скрипт

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
-document.getElementById("openbtn").addEventListener("click", function() {
-    var formSearch = document.getElementById("search-form");
-
+var formSearch = document.getElementById("search-form");
+document.getElementById("openbtn").addEventListener("click", function () {
     if (!formSearch.classList.contains("open")) {
         formSearch.classList.add("open");
     }
@@ -8,4 +7,17 @@ document.getElementById("openbtn").addEventListener("click", function() {
         formSearch.classList.remove("open");
     }
 });
-
+var submit = document.getElementById("send");
+submit.addEventListener("click", function (e) {
+    if (document.getElementById("arrival").value == "" || document.getElementById("departure").value == "" || document.getElementById("adults").value == "" || document.getElementById("children").value == "" ) {
+        e.preventDefault();
+        submit.classList.add("animate");
+        return false;
+    } 
+    else {
+        return true;
+    }
+});
+formSearch.addEventListener('change', function (e) {
+    submit.classList.remove("animate");
+});
